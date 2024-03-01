0c63548c7446ea93d349e1958097ffcbbf29763c
s27559 <161855502+s27559@users.noreply.github.com>
2024-03-01 16:19:55 +0100

    ex1 and ex2

---
+++ ex1.py
@@ -0,0 +1,4 @@
+name = input("input name: ")
+surname = input("input surname: ")
+age = input("input age: ")
+print("Hello, " + name + " " + surname + " " + age + "!")
---
+++ ex2.py
@@ -0,0 +1,2 @@
+fahr = float(input("input temperature in fahrenheit: "))
+cels = (fahr - 32) * (5/9)
